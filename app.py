42def64734e3f4420a56397d4da905f895853ace
simranbhatiawork09-cyber <simran.bhatiawork09@gmail.com>
2026-03-04 21:07:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,5 @@ def home():
     return "WhatsApp webhook is running!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
